849e802ae3d285dc283437fcbc37af73cbb41d56
Anandaswarup Vadapalli <anandaswarup@athena.local>
2019-07-31 12:15:32 +0530

    Convolutional Layers

--- layers/Convolution.py
+++ layers/Convolution.py
@@ -8,20 +8,24 @@ import torch.nn.functional as F
 class Conv1D(nn.Module):
     """1-D convolution layer with Xavier uniform initialization
     """
-    def __init__(self, in_channels, out_channels, kernel_size=1, stride=1, padding=None, dilation=1, bias=True,
-                 w_init_gain="linear"):
+    def __init__(self, in_channels, out_channels, kernel_size=1, stride=1, padding=None, bias=True,
+                 init_gain="linear"):
         """Constructor
         """
         super().__init__()
 
         if padding is None:
             assert kernel_size % 2 == 1
-            padding = dilation * (kernel_size - 1) // 2
+            padding = (kernel_size - 1) // 2
 
         self.conv = torch.nn.Conv1d(in_channels, out_channels, kernel_size=kernel_size, stride=stride,
-                                    padding=padding, dilation=dilation, bias=bias)
+                                    padding=padding, bias=bias)
+        self._init_weights(init_gain)
 
-        torch.nn.init.xavier_uniform_(self.conv.weight, gain=nn.init.calculate_gain(w_init_gain))
+    def _init_weights(self, init_gain):
+        """Initialize the weights of the convolutional layer
+        """
+        torch.nn.init.xavier_uniform_(self.conv.weight, gain=nn.init.calculate_gain(init_gain))
 
     def forward(self, inputs):
         """Forward pass
@@ -32,7 +36,7 @@ class Conv1D(nn.Module):
 class BatchNormConv1D(nn.Module):
     """1-D convolution layer + batchnorm layer + activation + dropout
     """
-    def __init__(self, in_channels, out_channels, kernel_size, activation=None, dropout=0.5, w_init_gain="linear"):
+    def __init__(self, in_channels, out_channels, kernel_size, activation=None, dropout=0.5, init_gain="linear"):
         """Constructor
         """
         super().__init__()
@@ -40,7 +44,7 @@ class BatchNormConv1D(nn.Module):
         self.activation = activation
         self.dropout = dropout
 
-        self.conv1d = Conv1D(in_channels, out_channels, kernel_size=kernel_size, w_init_gain=w_init_gain)
+        self.conv1d = Conv1D(in_channels, out_channels, kernel_size=kernel_size, init_gain=init_gain)
 
         self.batchnorm = nn.BatchNorm1d(out_channels)
 
