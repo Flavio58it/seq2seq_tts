2a7aa706579155ff037b0e5600b2ea171a5b2160
Anandaswarup Vadapalli <anandaswarup@athena.local>
2019-08-02 11:44:13 +0530

    seq2seq Decoder

--- seq2seq/Decoder.py
+++ seq2seq/Decoder.py
@@ -36,7 +36,7 @@ class Tacotron2Decoder(nn.Module):
         self.attention = LocationSensitiveAttention(dec_LSTMCell_size, memory_dim, attn_dim, location_filters,
                                                     location_kernel_size)
         self.acoustic_projection = Dense(dec_LSTMCell_size + memory_dim, target_dim, bias=True)
-        self.gate_projection = Dense(dec_LSTMCell_size + memory_dim, 1, bias=True)
+        self.gate_projection = Dense(dec_LSTMCell_size + memory_dim, 1, bias=True, init_gain="sigmoid")
 
     def _get_go_frame(self, memory):
         """Get all zeros frame to be used to start the decoding (as first input to the decoder)
@@ -58,8 +58,8 @@ class Tacotron2Decoder(nn.Module):
         self.LSTM_cell = [Variable(memory.data.new(batch_size, self.dec_LSTMCell_size).zero_())] *\
             self.num_LSTMCells
 
-        self.attention_weights = Variable(memory.data.new(batch_size, max_time).zero_())
-        self.attention_weights_cumulative = Variable(memory.data.new(batch_size, max_time).zero_())
+        self.alignment = Variable(memory.data.new(batch_size, max_time).zero_())
+        self.cumulative_alignments = Variable(memory.data.new(batch_size, max_time).zero_())
         self.attention_context = Variable(memory.data.new(batch_size, self.memory_dim).zero_())
 
     def parse_inputs(self, decoder_inputs):
@@ -70,19 +70,19 @@ class Tacotron2Decoder(nn.Module):
 
         return decoder_inputs
 
-    def parse_outputs(self, acoustic_outputs, gate_outputs, alignments):
+    def parse_outputs(self, outputs, stop_tokens, alignments):
         """Parses the outputs of the decoder for further processing
         """
         # alignments: (batch_size, max_time)
-        alignments = torch.stack(alignments).transpose(0, 1)
+        alignments = torch.stack(alignments).transpose(0, 1).contiguous()
 
         # acoustic outputs: (batch_size, max_time, target_dim)
-        acoustic_outputs = torch.stack(acoustic_outputs).transpose(0, 1).contiguous()
+        outputs = torch.stack(outputs).transpose(0, 1).contiguous()
 
-        # gate outputs
-        gate_outputs = torch.stack(gate_outputs).transpose(0, 1).contiguous()
+        # stop tokens
+        stop_tokens = torch.stack(stop_tokens).transpose(0, 1).contiguous()
 
-        return acoustic_outputs, gate_outputs, alignments
+        return outputs, stop_tokens, alignments
 
     def step(self, decoder_input, memory, mask=None, training=False):
         """Decoder step using stored decoder states, attention and memory
@@ -104,42 +104,41 @@ class Tacotron2Decoder(nn.Module):
             self.LSTM_cell[idx] = F.dropout(self.LSTM_cell[idx], self.dropout, training=training)
             cell_input = self.LSTM_hidden[idx]
 
-        attention_weights_cat = torch.cat((self.attention_weights.unsqueeze(-1),
-                                           self.attention_weights_cumulative.unsqueeze(-1)), dim=-1)
+        alignments_cat = torch.cat((self.alignment.unsqueeze(-1), self.cumulative_alignments.unsqueeze(-1)), dim=-1)
 
-        self.attention_context, self.attention_weights = self.attention(self.LSTM_hidden[-1], memory,
-                                                                        attention_weights_cat, mask=mask)
+        self.attention_context, self.alignment = self.attention(self.LSTM_hidden[-1], memory, alignments_cat,
+                                                                mask=mask)
 
-        self.attention_weights_cumulative += self.attention_weights
+        self.cumulative_alignments += self.alignment
 
         cell_output = torch.cat((self.LSTM_hidden[-1], self.attention_context), dim=-1)
 
         acoustic_frame = self.acoustic_projection(cell_output)
-        gate = self.gate_projection(cell_output)
+        stop_token = self.gate_projection(cell_output)
 
-        return acoustic_frame, gate, self.attention_weights
+        return acoustic_frame, stop_token, self.alignment
 
-    def forward(self, decoder_inputs, memory, memory_lengths):
+    def forward(self, inputs, memory, memory_lengths):
         """Forward pass for training
         """
         go_frame = self._get_go_frame(memory).unsqueeze(1)
-        decoder_inputs = torch.cat((go_frame, decoder_inputs), dim=1)
-        decoder_inputs = self.parse_inputs(decoder_inputs)
+        inputs = torch.cat((go_frame, inputs), dim=1)
+        inputs = self.parse_inputs(inputs)
 
         self._init_states(memory)
 
-        acoustic_outputs, gate_outputs, alignments = [], [], []
-        while len(acoustic_outputs) < decoder_inputs.size(0) - 1:
-            inputs = decoder_inputs[len(acoustic_outputs)]
-            acoustic_frame, gate, attention_weights = self.step(inputs, memory, mask=get_mask(memory_lengths),
-                                                                training=True)
-            acoustic_outputs += [acoustic_frame.squeeze(1)]
-            gate_outputs += [gate.squeeze(1)]
-            alignments += [attention_weights]
+        outputs, stop_tokens, alignments = [], [], []
+        while len(outputs) < inputs.size(0) - 1:
+            inputs = inputs[len(outputs)]
+            acoustic_frame, stop_token, alignment = self.step(inputs, memory, mask=get_mask(memory_lengths),
+                                                              training=True)
+            outputs += [acoustic_frame.squeeze(1)]
+            stop_tokens += [stop_token.squeeze(1)]
+            alignments += [alignment]
 
-        acoustic_outputs, gate_outputs, alignments = self.parse_outputs(acoustic_outputs, gate_outputs, alignments)
+        outputs, stop_tokens, alignments = self.parse_outputs(outputs, stop_tokens, alignments)
 
-        return acoustic_outputs, gate_outputs, alignments
+        return outputs, stop_tokens, alignments
 
     def inference(self, memory):
         """Inference
@@ -148,23 +147,24 @@ class Tacotron2Decoder(nn.Module):
 
         self._init_states(memory)
 
-        acoustic_outputs, gate_outputs, alignments = [], [], []
+        outputs, stop_tokens, alignments = [], [], []
+
         while True:
-            acoustic_frame, gate, attention_weights = self.step(inputs, memory, mask=None, training=False)
+            acoustic_frame, stop_token, alignment = self.step(inputs, memory, mask=None, training=False)
 
-            acoustic_outputs += [acoustic_frame.squeeze(1)]
-            gate_outputs += [gate]
-            alignments += [attention_weights]
+            outputs += [acoustic_frame.squeeze(1)]
+            stop_tokens += [stop_token.squeeze(1)]
+            alignments += [alignment]
 
-            if torch.sigmoid(gate.data) >= 0.5:
+            if torch.sigmoid(stop_token.data) >= 0.5:
                 break
 
-            if len(acoustic_outputs) == self.max_decoder_steps:
+            if len(outputs) == self.max_decoder_steps:
                 print("Warning! Reached max decoder steps")
                 break
 
             inputs = acoustic_frame
 
-        acoustic_outputs, gate_outputs, alignments = self.parse_outputs(acoustic_outputs, gate_outputs, alignments)
+        outputs, stop_tokens, alignments = self.parse_outputs(outputs, stop_tokens, alignments)
 
-        return acoustic_outputs, gate_outputs, alignments
+        return outputs, stop_tokens, alignments
