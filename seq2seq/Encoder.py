c4aef0fb26e2983171f962fd3d80f35c386c99c0
Anandaswarup Vadapalli <anandaswarup@athena.local>
2019-08-02 11:44:06 +0530

    seq2seq Encoder

--- seq2seq/Encoder.py
+++ seq2seq/Encoder.py
@@ -1,6 +1,6 @@
 """seq2seq Encoder"""
 
-import torch.nn.functional as F
+import torch
 import torch.nn as nn
 
 from layers.Convolution import BatchNormConv1D
@@ -16,12 +16,12 @@ class Tacotron2Encoder(nn.Module):
 
         filters = [in_dim] + num_conv_layers * [conv_filters]
         self.convs = nn.ModuleList([BatchNormConv1D(in_channels, out_channels, kernel_size=conv_kernel_size,
-                                    activation=F.relu, dropout=dropout) for in_channels, out_channels in
+                                    activation=torch.relu, dropout=dropout) for in_channels, out_channels in
                                     zip(filters, filters[1:])])
         self.BLSTM = nn.LSTM(conv_filters, BLSTM_size//2, num_layers=1, bias=True, batch_first=True,
                              bidirectional=True)
 
-    def forward(self, inputs, input_lengths, max_input_length):
+    def forward(self, inputs, input_lengths):
         """Forward pass
         """
         inputs = inputs.transpose(1, 2)
@@ -32,12 +32,12 @@ class Tacotron2Encoder(nn.Module):
         inputs = inputs.transpose(1, 2)
 
         input_lengths = input_lengths.cpu().numpy()
-        inputs = nn.utils.rnn.pack_padded_sequence(inputs, input_lengths, batch_first=True, enforce_sorted=False)
+        inputs = nn.utils.rnn.pack_padded_sequence(inputs, input_lengths, batch_first=True)
 
         self.BLSTM.flatten_parameters()
         outputs, _ = self.BLSTM(inputs)
 
-        outputs, _ = nn.utils.rnn.pad_packed_sequence(outputs, batch_first=True, total_length=max_input_length)
+        outputs, _ = nn.utils.rnn.pad_packed_sequence(outputs, batch_first=True)
 
         return outputs
 
