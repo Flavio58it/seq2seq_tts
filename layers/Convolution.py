9da761cb9f20c81dee7a6065593fa0368b3f4906
Anandaswarup Vadapalli <anandaswarup@athena.local>
2019-08-18 16:22:11 +0530

    Convolution layers

--- layers/Convolution.py
+++ layers/Convolution.py
@@ -1,61 +1,57 @@
 """Convolution layers"""
 
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
+import tensorflow as tf
 
 
-class Conv1D(nn.Module):
+class Conv1D(tf.keras.Model):
     """1-D convolution layer with Xavier uniform initialization
     """
-    def __init__(self, in_channels, out_channels, kernel_size=1, stride=1, padding=None, bias=True,
-                 init_gain="linear"):
+    def __init__(self, filters, kernel_size=1, stride=1, dilation=1, bias=True):
         """Constructor
         """
         super().__init__()
 
-        if padding is None:
-            assert kernel_size % 2 == 1
-            padding = (kernel_size - 1) // 2
+        self.filters = filters
+        self.kernel_size = kernel_size
+        self.stride = stride
+        self.dilation = dilation
+        self.bias = bias
 
-        self.conv = torch.nn.Conv1d(in_channels, out_channels, kernel_size=kernel_size, stride=stride,
-                                    padding=padding, bias=bias)
-        self._init_weights(init_gain)
+        self.conv = tf.keras.layers.Conv1D(filters=filters, kernel_size=kernel_size, strides=stride, padding="same",
+                                           data_format="channels_last", dilation_rate=dilation, activation=None,
+                                           use_bias=bias, kernel_initializer="glorot_uniform")
 
-    def _init_weights(self, init_gain):
-        """Initialize the weights of the convolutional layer
-        """
-        torch.nn.init.xavier_uniform_(self.conv.weight, gain=nn.init.calculate_gain(init_gain))
-
-    def forward(self, inputs):
+    def call(self, inputs):
         """Forward pass
         """
         return self.conv(inputs)
 
 
-class BatchNormConv1D(nn.Module):
+class BatchNormConv1D(tf.keras.Model):
     """1-D convolution layer + batchnorm layer + activation + dropout
     """
-    def __init__(self, in_channels, out_channels, kernel_size, activation=None, dropout=0.5, init_gain="linear"):
+    def __init__(self, filters, kernel_size, activation=None, dropout=0.5):
         """Constructor
         """
         super().__init__()
 
+        self.filters = filters
+        self.kernel_size = kernel_size
         self.activation = activation
         self.dropout = dropout
 
-        self.conv1d = Conv1D(in_channels, out_channels, kernel_size=kernel_size, init_gain=init_gain)
+        self.conv = Conv1D(filters=filters, kernel_size=kernel_size)
+        self.batchnorm = tf.keras.layers.BatchNormalization(axis=-1)
+        self.dropout = tf.keras.layers.Dropout(rate=dropout)
 
-        self.batchnorm = nn.BatchNorm1d(out_channels)
-
-    def forward(self, inputs, training=False):
+    def call(self, inputs, training=False):
         """Forward pass
         """
-        inputs = self.batchnorm(self.conv1d(inputs))
+        inputs = self.batchnorm(self.conv(inputs))
 
         if self.activation is not None:
             inputs = self.activation(inputs)
 
-        inputs = F.dropout(inputs, p=self.dropout, training=training)
+        inputs = self.dropout(inputs, training=training)
 
         return inputs
